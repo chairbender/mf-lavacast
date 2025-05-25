33108bb350de4e1f6cb987a92ad7356a586be7a2
chairbender <kwhipke1@gmail.com>
2025-05-25 15:55:29 -0700

    add note

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mf-lavacast
 Mineflayer script to lavacast a pyramid.
+
+Currently blocked by https://github.com/PrismarineJS/mineflayer/issues/3641 
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import Mineflayer from 'mineflayer'
+const { mineflayer: mineflayerViewer } = require('prismarine-viewer')
+
 
 const bot = Mineflayer.createBot({
-  host: 'localhost',
-  port: 37811,
-  username: 'Bloomp',
+  host: 'percypanic.apexmc.co',
+  username: 'ForTheUSSR',
+  auth: "microsoft"
+})
+
+bot.once('spawn', () => {
+  mineflayerViewer(bot, { port: 3007, firstPerson: true }) // port is the viewing server port, if first person is false, you get a bird's-eye view
 })
 
 bot.on('chat', (username, message) => {
